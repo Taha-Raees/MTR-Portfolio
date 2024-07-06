5d748547a1b06cfa94f01a2f9b5d3c0c3dd41ef1
Muhammad Taha Raees <raeesmuhammadtaha@gmail.com>
2024-07-07 00:01:47 +0100

    google-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import type { Metadata } from "next";
+// src/layouts/RootLayout.js
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "MTR Portfolio",
   description: "Muhammad Taha Raees portfolio showcasing my projects and my education",
 };
@@ -16,6 +18,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+// src/pages/Home.js
+"use client"
+import { useEffect } from 'react';
 import Education from "@/components/Education";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import ParticleEffect from "@/components/ParticleEffect";
 import CustomCursor from "@/components/CustomCursor";
+import * as gtag from '../lib/gtag';
 
 export default function Home() {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    handleRouteChange(window.location.pathname);
+    window.addEventListener('popstate', () => handleRouteChange(window.location.pathname));
+
+    return () => {
+      window.removeEventListener('popstate', () => handleRouteChange(window.location.pathname));
+    };
+  }, []);
+
   return (
     <div className="relative">
       <CustomCursor />
